d36db3793422f49bb0244f782440ae3f51814d66
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-03 10:40:44 +0200

    Updated routes.ts

--- cybertalks/start/routes.ts
+++ cybertalks/start/routes.ts
@@ -12,13 +12,9 @@ import router from '@adonisjs/core/services/router'
 const EventsController = () => import("#controllers/events_controller")
 
 router.on('/').render('pages/home').as('home')
-
 router.on('/categories').render('pages/categories').as('categories')
-
 router.on('/categories/:id').render('pages/category').as('category')
-
 router.on('/event/:id').render('pages/event').as('event')
-
 router.on('/contact').render('pages/contact').as('contact')
 
 router.get('/event', [EventsController, 'eventShow'])
