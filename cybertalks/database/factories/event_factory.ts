412bc2b4f13d2aef4fe1d0903e12320e16a8692c
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-03 15:24:12 +0200

    Updated event_factory.ts

--- cybertalks/database/factories/event_factory.ts
+++ cybertalks/database/factories/event_factory.ts
@@ -1,3 +1,4 @@
+import { DateTime } from 'luxon'
 import factory from '@adonisjs/lucid/factories'
 import Event from '#models/event'
 
@@ -7,8 +8,8 @@ export const EventFactory = factory
       title: faker.lorem.sentence(),
       content: faker.lorem.paragraphs(4),
       isOnline: faker.datatype.boolean(50),
-      date: faker.date.future(),
-      speakers: faker.datatype.number({ min: 4, max: 10 })
+      date: DateTime.now(),
+      speakers: faker.number.int({ min: 1, max: 10 }),
     }
   })
   .build()
