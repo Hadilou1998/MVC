643a071f377f1bb4b1b17c4cf3aadb52107c7998
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-03 10:36:29 +0200

    Updated routes.ts

--- cybertalks/start/routes.ts
+++ cybertalks/start/routes.ts
@@ -6,8 +6,10 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
 import router from '@adonisjs/core/services/router'
+
+// On charge le controller
+const EventsController = () => import("#controllers/events_controller")
 
 router.on('/').render('pages/home').as('home')
 
@@ -18,3 +20,5 @@ router.on('/categories/:id').render('pages/category').as('category')
 router.on('/event/:id').render('pages/event').as('event')
 
 router.on('/contact').render('pages/contact').as('contact')
+
+router.get('/event', [EventsController, 'eventShow'])
