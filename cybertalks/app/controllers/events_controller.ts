09db6b5441c62cc235af7ff7b65302bfd65f3e50
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-03 09:56:49 +0200

    Updated events_controller.ts

--- cybertalks/app/controllers/events_controller.ts
+++ cybertalks/app/controllers/events_controller.ts
@@ -1,6 +1,4 @@
-// import type { HttpContext } from '@adonisjs/core/http'
-
-import { HttpContext } from "@adonisjs/core/http";
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class EventsController {
     async eventShow({ view }: HttpContext) {
