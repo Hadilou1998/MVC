0439de7e25ab34935728461bbb47336693d4eadc
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-03 14:34:04 +0200

    Updated event_seeder.ts

--- cybertalks/database/seeders/event_seeder.ts
+++ cybertalks/database/seeders/event_seeder.ts
@@ -1,7 +1,19 @@
+import User from '#models/user'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
-    // Write your database queries inside the run method
+    await User.createMany([
+      {
+        fullName: 'virk',
+        email: 'virk@adonisjs.com',
+        password: 'secret',
+      },
+      {
+        fullName: 'romain',
+        email: 'romain@adonisjs.com',
+        password: 'supersecret',
+      },
+    ])
   }
 }
